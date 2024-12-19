9a87d1422ef4ee5930f86232e810d0b1aeb57ba5
Rohit <rohitmalla118@gmail.com>
2024-12-19 22:21:26 +0530

    put

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,20 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 export default defineConfig({
   plugins: [svelte()],
+  build: {
+    outDir: 'public',
+  },
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000', // Your backend server address
+        target: 'https://usermanagementsystem-z2sv.onrender.com', // Your backend server address
         changeOrigin: true,
       },
     },
   },
 })
+
+
+
+
+
